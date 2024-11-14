1b354e837f81587921c0f39a90eab33bfa3f2e1d
Guido Filho <guido.warken@gmail.com>
2024-11-14 10:06:37 -0300

    abc

--- api/index.js
+++ api/index.js
@@ -42,4 +42,8 @@ app.post("/api/videos", (request, response) => {
   response.status(201);
 });
 
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log("Server is listening"));
+
 module.exports = app;
