4a6859f743a779e99b8217cfd29b2cec5de7102b
Guido Filho <guido.warken@gmail.com>
2024-11-13 22:58:07 -0300

    Creates a express rest api for the videos

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const fs = require("fs");
+
+const videosData = require("./data/videos.json");
+
+const envFile = `.env.${process.env.NODE_ENV || "development"}`;
+
+dotenv.config({ path: envFile });
+
+const { videos } = videosData;
+
+const app = express();
+
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+
+app.use(express.json());
+
+app.get("/api/videos", (request, response) => response.json(videos));
+
+app.get("/api/videos/:id", (request, response) => {
+  const id = parseInt(request.params.id, 10);
+  const video = videos.find((v) => v.id === id);
+
+  if (video) {
+    response.json(video);
+  } else {
+    response.status(404);
+  }
+});
+
+app.post("/api/videos", (request, response) => {
+  const novoVideo = request.body;
+
+  videos.push(novoVideo);
+
+  const jsonString = JSON.stringify({ videos }, null, 2);
+
+  fs.writeFileSync("./data/videos.json", jsonString);
+
+  response.status(201);
+});
+
+app.listen(3000, () => console.log("Server is listening on port 3000"));
